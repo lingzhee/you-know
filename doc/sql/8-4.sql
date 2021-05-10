5158c37c26b82b7585a8e551bf6ac4946755484b
lingzhee <40168641+lingzhee@users.noreply.github.com>
2021-05-10 10:34:09 +0800

    Delete doc directory

--- doc/sql/8-4.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE `order_product`
-	ADD COLUMN `category_ids` VARCHAR(200) NULL DEFAULT NULL AFTER `supply_price`;
-
-
-
-ALTER TABLE `coupon_receive_log`
-	ADD COLUMN `state` VARCHAR(50) NULL DEFAULT 'Y' COMMENT 'Y可领取 N已失效' AFTER `member_id`;
